b344e1b5a8e7f2d0d23c59d051d1fa3e83a20962
Gabriel Duque <gabriel.duque@profusion.mobi>
2023-05-22 21:48:31 -0300

    feat: first content checking possibilities with JAVA 8

---
+++ src/main/java/Loja.java
@@ -0,0 +1,65 @@
+public class Loja {
+    private String nome;
+    private String email;
+    private String senha;
+    private String cpfCnpj;
+    private String endereco;
+
+    public Loja(String nome, String email, String senha, String cpfCnpj, String endereco) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpfCnpj = cpfCnpj;
+        this.endereco = endereco;
+    }
+
+    // Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    // Método para exibir os dados da loja
+    public void exibirLoja() {
+        System.out.println("Nome: " + nome);
+        System.out.println("E-mail: " + email);
+        System.out.println("CPF/CNPJ: " + cpfCnpj);
+        System.out.println("Endereço: " + endereco);
+    }
+}
