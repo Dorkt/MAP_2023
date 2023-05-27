98d76bc15c520b428763f4f20daf762085b7db2a
ArthurSalesFM <sales.arthur.fm@gmail.com>
2023-05-27 12:48:08 -0300

    Atualizando a classe Produto, com variáveis, Gets e Sets

--- src/main/java/Produto.java
+++ src/main/java/Produto.java
@@ -1,3 +1,69 @@
 public class Produto{
+    // Variables Declaration
+    private String nome;
+    private double valor;    
+    private int tipo;
+    private int quantidade;
+    private String marca;
+    private String descricao;    
+
+    // Generic Constructor
+    public Produto(String nome, double valor, int tipo, int quantidade, String marca, String descricao){
+        this.nome = nome;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.quantidade = quantidade;
+        this.marca = marca;
+        this.descricao = descricao;
+    }
     
+    // Gets and Sets
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public double getValor(){
+        return this.valor;
+    }
+
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+
+    public int getTipo(){
+        return this.tipo;
+    }
+
+    public void setTipo(int tipo){
+        this.tipo = tipo;
+    }
+
+    public int getQuantidade(){
+        return this.quantidade;
+    }
+
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
 }
