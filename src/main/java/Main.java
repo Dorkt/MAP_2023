b11262b440674f9319e24906660083acce4d9fad
ArthurSalesFM <sales.arthur.fm@gmail.com>
2023-05-27 16:07:43 -0300

    Enviando os menus parciais

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,17 @@
-import Comprador.Comprador;
-import Loja.Loja;
+//import Comprador.Comprador;
+//import Loja.Loja;
+
+import Menus.MenuPrincipal;
 
 public class Main {
     public static void main(String[] args) {
         // Criando uma loja
-        Loja loja = new Loja("Minha Loja", "loja@email.com", "senha123", "123456789", "Rua A");
+        //Loja loja = new Loja("Minha Loja", "loja@email.com", "senha123", "123456789", "Rua A");
         // Exibindo os dados da loja
-        loja.exibirLoja();
-        Comprador comprador = new Comprador("João", "joao@email.com", "senha456", "987654321", "Rua B");
-        comprador.exibirComprador();
+        //loja.exibirLoja();
+        //Comprador comprador = new Comprador("João", "joao@email.com", "senha456", "987654321", "Rua B");
+        //comprador.exibirComprador();
+
+        MenuPrincipal mp = new MenuPrincipal();
     }
 }
--- src/main/java/Menus/MenuPrincipal.java
+++ src/main/java/Menus/MenuPrincipal.java
@@ -54,6 +54,7 @@ public class MenuPrincipal {
             break;
 
             default:
+            System.out.println("Digite uma opão válida....");
         }
     }
 
