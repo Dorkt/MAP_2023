575b7e603b8783bd07f00824db0e251ca0f104ad
LaraYasmin <lyasminoliveira18@gmail.com>
2023-05-30 10:45:08 -0300

    Novo update

--- src/main/java/Comprador.java
+++ src/main/java/Comprador.java
@@ -1,11 +1,13 @@
 public class Comprador {
+    private int id;
     private String nome;
     private String email;
     private String senha;
     private String cpf;
-    private String endereco;
+    private Endereco endereco;
 
-    public Comprador(String nome, String email, String senha, String cpf, String endereco) {
+    public Comprador(int id, String nome, String email, String senha, String cpf, Endereco endereco) {
+        this.id = id;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
@@ -14,6 +16,14 @@ public class Comprador {
     }
 
     // Getters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
@@ -47,16 +57,17 @@ public class Comprador {
         this.cpf = cpf;
     }
 
-    public String getEndereco() {
+    public Endereco getEndereco() {
         return endereco;
     }
 
-    public void setEndereco(String endereco) {
+    public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
 
     // Método para exibir os dados do comprador
     public void exibirComprador() {
+        System.out.println("ID: " + id);
         System.out.println("Nome: " + nome);
         System.out.println("E-mail: " + email);
         System.out.println("CPF: " + cpf);
--- src/main/java/Loja.java
+++ src/main/java/Loja.java
@@ -1,11 +1,13 @@
 public class Loja {
+    private int id;
     private String nome;
     private String email;
     private String senha;
     private String cpfCnpj;
-    private String endereco;
+    private Endereco endereco;
 
-    public Loja(String nome, String email, String senha, String cpfCnpj, String endereco) {
+    public Loja(int id, String nome, String email, String senha, String cpfCnpj, Endereco endereco) {
+        this.id = id;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
@@ -14,6 +16,14 @@ public class Loja {
     }
 
     // Getters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
@@ -47,16 +57,17 @@ public class Loja {
         this.cpfCnpj = cpfCnpj;
     }
 
-    public String getEndereco() {
+    public Endereco getEndereco() {
         return endereco;
     }
 
-    public void setEndereco(String endereco) {
+    public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
 
     // Método para exibir os dados da loja
     public void exibirLoja() {
+        System.out.println("ID: " + id);
         System.out.println("Nome: " + nome);
         System.out.println("E-mail: " + email);
         System.out.println("CPF/CNPJ: " + cpfCnpj);
