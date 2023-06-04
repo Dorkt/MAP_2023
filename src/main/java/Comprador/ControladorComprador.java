920b7219a23df7a84955eae25c31383342909969
Gabriel Duque <gabrieldorkt@hotmail.com>
2023-06-04 14:54:39 -0300

    feat: create heritage controller

---
+++ src/main/java/Comprador/ControladorComprador.java
@@ -0,0 +1,6 @@
+package Comprador;
+
+import Controlador.Controlador;
+
+public class ControladorComprador extends Controlador {
+}
---
+++ src/main/java/Produto/ControladorProduto.java
@@ -0,0 +1,6 @@
+package Produto;
+
+import Controlador.Controlador;
+
+public class ControladorProduto extends Controlador {
+}
