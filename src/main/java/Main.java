02a25e1f7007ddae6247bc93048ca39792d0e90e
Luiz Carlos <luizlemos02@gmail.com>
2023-06-03 22:50:28 -0300

    Atualizações

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,20 +18,5 @@ public class Main {
         //comprador.exibirComprador();
 
         //MenuPrincipal mp = new MenuPrincipal();
-
-        ControladorLoja controle1 = new ControladorLoja();
-        Loja loja = new Loja("Lojão 10", "lojao10@gmail.com", "101010", "001.234.0005-67", "Rua do Bar, 10");
-        controle1.persistirLoja(loja);
-        System.out.println(controle1.pegarTodasLojas());
-
-        ControladorComprador controle2 = new ControladorComprador();
-        Comprador comprador = new Comprador("José", "jose@gmail.com", "123456", "123.456.789-10", "Avenida Vazia, 50");
-        controle2.persistirComprador(comprador);
-        System.out.println(controle2.pegarTodosCompradores());
-
-        ControladorProduto controle3 = new ControladorProduto();
-        Produto produto = new Produto("Borracha", 5.99, 5, 1, "Toque Mágico", "Borracha Branca Macia");
-        controle3.persistirProduto(produto);
-        System.out.println(controle3.pegarTodosProdutos());
     }
 }
