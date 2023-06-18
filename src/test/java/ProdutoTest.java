57c139643ab94d2d45e9ac36e55fc8d6e8723e7e
LaraYasmin <lyasminoliveira18@gmail.com>
2023-06-18 17:24:45 -0300

    New tests

---
+++ src/test/java/ProdutoTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import Produto.Produto;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProdutoTest {
+
+    @Test
+    public void testConstructorAndGetters() {
+        Produto produto = new Produto("Nome do Produto", 10.0, "Tipo do Produto", 5, "Marca", "Descrição");
+        assertEquals("Nome do Produto", produto.getNome());
+        assertEquals(10.0, produto.getValor());
+        assertEquals("Tipo do Produto", produto.getTipo());
+        assertEquals(5, produto.getQuantidade());
+        assertEquals("Marca", produto.getMarca());
+        assertEquals("Descrição", produto.getDescricao());
+    }
+
+    @Test
+    public void testSetters() {
+        Produto produto = new Produto();
+        produto.setNome("Nome do Produto");
+        produto.setValor(10.0);
+        produto.setTipo("Tipo do Produto");
+        produto.setQuantidade(5);
+        produto.setMarca("Marca");
+        produto.setDescricao("Descrição");
+
+        assertEquals(0, produto.getId());
+        assertEquals("Nome do Produto", produto.getNome());
+        assertEquals(10.0, produto.getValor());
+        assertEquals("Tipo do Produto", produto.getTipo());
+        assertEquals(5, produto.getQuantidade());
+        assertEquals("Marca", produto.getMarca());
+        assertEquals("Descrição", produto.getDescricao());
+    }
+
+    @Test
+    public void testExibirProduto() {
+        Produto produto = new Produto( "Nome do Produto", 10.0, "Tipo do Produto", 5, "Marca", "Descrição");
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        produto.exibirProduto();
+
+        System.setOut(System.out);
+
+        String expectedOutput = "ID: 1\nNome: Nome do Produto\nValor: R$10.0\nTipo de produto Tipo do Produto\nQuantidade: 5\nMarca: Marca\nDescrição: Descrição\n";
+        assertNotEquals(expectedOutput, outContent.toString());
+    }
+}
